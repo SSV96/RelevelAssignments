c5a539195876fd0903d7710eb9d01a1e8144c733
SwaroopLightning <98945513+SwaroopLightning@users.noreply.github.com>
2022-02-03 16:06:21 +0530

    Update Basic Calculator.js

--- Basic Calculator.js
+++ Basic Calculator.js
@@ -1,6 +1,9 @@
 
-// 1. Create a basic calculator program using Javascript
-// 2. Use different arithmetic operators and variables to show working of calculator
+// 1. Create a basic calculator program using Javascript.
+// 2. Use different arithmetic operators and variables to show working of calculator.
+// 3.This code will work on browser console.
+// 4.Copy this code and paste in browser console.
+
 console.log(" welcome to calculator ")
 console.log(" Please Enter value of A  ")
 
@@ -16,6 +19,7 @@ console.log('a*b = ',a*b);//
 console.log('a/b = ',a/b);//
 
 console.log("Enter  Radius of circle") 
-let r = parseInt(prompt()) ;// r is for Radius of a circle
+let r = parseInt(prompt()) ;// r is for Radius of a circle.
 
-console.log("Area of circle ",pi*(r*r),"units"); // pi is const variable 
+console.log("Area of circle ",pi*(r*r),"units"); // pi is const variable.
+
