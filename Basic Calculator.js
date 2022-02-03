cfb2d5238142a9c28cd59e190566a57458a6d6a8
SwaroopLightning <98945513+SwaroopLightning@users.noreply.github.com>
2022-02-03 16:05:02 +0530

    Update Basic Calculator.js

--- Basic Calculator.js
+++ Basic Calculator.js
@@ -1,7 +1,21 @@
-const a = parseInt(prompt());
+
+// 1. Create a basic calculator program using Javascript
+// 2. Use different arithmetic operators and variables to show working of calculator
+console.log(" welcome to calculator ")
+console.log(" Please Enter value of A  ")
+
+let a = parseInt(prompt());//using let, parseInt() is a function which converts string to int.
+
+console.log(" Please Enter value of B  ")
 let b   = parseInt(prompt());
 
-console.log('a+b = ',a+b);
-console.log('a-b = ',a-b);
-console.log('a*b = ',a*b);
-console.log('a/b = ',a/b);
+const pi = 3.142;//using const 
+console.log('a+b = ',a+b);//
+console.log('a-b = ',a-b);//
+console.log('a*b = ',a*b);//
+console.log('a/b = ',a/b);//
+
+console.log("Enter  Radius of circle") 
+let r = parseInt(prompt()) ;// r is for Radius of a circle
+
+console.log("Area of circle ",pi*(r*r),"units"); // pi is const variable 
